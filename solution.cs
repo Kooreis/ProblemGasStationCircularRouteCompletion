69ce2f673cf5fa97751309a216d078556b146b7c
Kooreis <kooreis.official@gmail.com>
2024-11-15 06:16:10 -0600

    Adds a for loop that iterates over the gas array. It updates totalGas and totalCost by adding the current gas and cost values. It also updates the tank value by subtracting the current cost from the current gas. If the tank value is less than 0, it updates the start value to the next index and resets the tank value to 0.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,9 @@
-function canCompleteCircuit(gas, cost) {
-    let totalGas = 0;
-    let totalCost = 0;
-    let tank = 0;
-    let start = 0;
-}
+for (let i = 0; i < gas.length; i++) {
+        totalGas += gas[i];
+        totalCost += cost[i];
+        tank += gas[i] - cost[i];
+        if (tank < 0) {
+            start = i + 1;
+            tank = 0;
+        }
+    }
